37ac5500165dc5f674af18662f939c5e66a30f6d
Jhonata <gutemberg.jhonata@gmail.com>
2023-05-12 16:23:01 -0300

    refactor(payroll): refactoring methods with final

--- src/main/java/tech/gutinho/domain/usecases/CalculatePayrollUseCase.java
+++ src/main/java/tech/gutinho/domain/usecases/CalculatePayrollUseCase.java
@@ -7,15 +7,15 @@ import tech.gutinho.domain.entities.Payslip;
 public class CalculatePayrollUseCase {
     public static final double EXTRA_PERCENTUAL_VALUE = 0.1;
 
-    public Payslip calculate(double hoursWorked, EmploymentContract employmentContract) {
+    public Payslip calculate(final double hoursWorked, final EmploymentContract employmentContract) {
         return calculate(hoursWorked, 0, employmentContract);
     }
 
-    public Payslip calculate(double hoursWorked, double overtimeWorked, EmploymentContract employmentContract) {
-        double totalHoursValue = calculateTotalHoursValue(hoursWorked, employmentContract);
-        double totalOvertimeValue = calculateTotalOvertimeValue(overtimeWorked, employmentContract);
-        double totalValue = totalHoursValue + totalOvertimeValue;
-        double extraValue = calculateExtraValue(totalValue, employmentContract.getEmployee());
+    public Payslip calculate(final double hoursWorked, final double overtimeWorked, final EmploymentContract employmentContract) {
+        final double totalHoursValue = calculateTotalHoursValue(hoursWorked, employmentContract);
+        final double totalOvertimeValue = calculateTotalOvertimeValue(overtimeWorked, employmentContract);
+        final double totalValue = totalHoursValue + totalOvertimeValue;
+        final double extraValue = calculateExtraValue(totalValue, employmentContract.getEmployee());
         Payslip payslip = new Payslip();
         payslip.setTotalHoursValue(totalHoursValue);
         payslip.setTotalOvertimeValue(totalOvertimeValue);
@@ -23,15 +23,15 @@ public class CalculatePayrollUseCase {
         return payslip;
     }
 
-    public double calculateTotalHoursValue(double hoursWorked, EmploymentContract employmentContract) {
+    public double calculateTotalHoursValue(final double hoursWorked, final EmploymentContract employmentContract) {
         return employmentContract.getHoursValue() * hoursWorked;
     }
 
-    private double calculateTotalOvertimeValue(double overtimeWorked, EmploymentContract employmentContract) {
+    private double calculateTotalOvertimeValue(final double overtimeWorked, final EmploymentContract employmentContract) {
         return employmentContract.getOvertimeValue() * overtimeWorked;
     }
 
-    private double calculateExtraValue(double totalValue, Employee employee) {
+    private double calculateExtraValue(final double totalValue, final Employee employee) {
         return employee.hasChildren() ? totalValue * EXTRA_PERCENTUAL_VALUE : 0;
     }
 }
